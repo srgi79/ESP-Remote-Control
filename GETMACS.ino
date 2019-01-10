02330ff5c9c53158d3c47efd5005ef459103e142
srgi79 <36154007+srgi79@users.noreply.github.com>
2019-01-10 19:20:10 +0100

    Add files via upload
    ESP32 included

--- GETMACS.ino
+++ GETMACS.ino
@@ -1,4 +1,9 @@
-#include <ESP8266WiFi.h>  
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>        
+#else
+#include <WiFi.h>
+#endif
+  
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_AP_STA);
